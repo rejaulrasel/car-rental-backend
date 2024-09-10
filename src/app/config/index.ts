10e8e51507d65ffb3a62b9b75fd9f00f469dd18e
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-09-10 17:10:16 +0600

    added basic middleware and utils function

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,4 +6,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 };
